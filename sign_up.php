44f02b657e6df1187b53e8049abf310ce074f45a
Abhay Gupta <abhay.abhaygupta.gupta3@gmail.com>
2017-10-09 17:55:22 +0530

    Sign-up page

--- sign_up.php
+++ sign_up.php
@@ -1,4 +1,8 @@
 <?php include 'connection.php' ?>
+<?php 
+    session_start();
+    $_SESSION['auth']=0;
+?>
 
 <!doctype HTML>
 <html>
@@ -14,7 +18,13 @@
                 <div class="login"><a href="sorter_login.php">Tester Login</a></div>
             </div>
         </header>
-        <br><br><br><br><br><br><br>
-        <a href="sign_up.php">Sign Up</a>
+        <br><br><br><br><br><br><br><br>
+        <form method="POST" action="sign_up_process.php">
+        <label for aadhar>Aadhar Id: <input type="text" name="aadhar"></label><br>
+        <label for dob>DOB: <input type="date" name="dob"></label><br>
+        <label for pass>Password: <input type="password" name="pass"></label><br>
+        <label for cpass>Confirm Password: <input type="password" name="cpass"></label><br>
+        <input type="submit" value="Submit">
+        </form>
     </body>
 </html>
--- sign_up_process.php
+++ sign_up_process.php
@@ -2,18 +2,23 @@
 
 <?php
     session_start();
-    $_SESSION['user']=$_POST['user'];
+    $_SESSION['aadhar']=$_POST['aadhar'];
     $_SESSION['pass']=$_POST['pass'];
+    $_SESSION['cpass']=$_POST['cpass'];
+    $_SESSION['dob']=$_POST['dob'];
+    
    
-    if(isset($_SESSION['user']) && isset($_SESSION['pass']) && $_SESSION['user']!="" && $_SESSION['pass']!="" && $_SESSION['auth']==0){
+    if(isset($_SESSION['aadhar']) && isset($_SESSION['pass']) && $_SESSION['aadhar']!="" && $_SESSION['pass']!="" && isset($_SESSION['cpass']) && $_SESSION['cpass']!="" && isset($_SESSION['dob']) && $_SESSION['dob']!="" && $_SESSION['auth']==0){
         
         $_SESSION['auth']=1;
-        $user=mysqli_real_escape_string($con,$_SESSION['user']);
+        $aadhar=mysqli_real_escape_string($con,$_SESSION['aadhar']);
 
         $pass=mysqli_real_escape_string($con,$_SESSION['pass']);
+        $cpass=mysqli_real_escape_string($con,$_SESSION['cpass']);
+        $dob=mysqli_real_escape_string($con,$_SESSION['dob']);
 
 
-        $query="SELECT Aadhar_id,AES_DECRYPT(Password,'AbhayGuptaPathologyLab') AS Password FROM patient;";
+        $query="SELECT Aadhar_id,DOB FROM patient where Aadhar_id='$aadhar' AND DOB='$dob' ;";
         if(!mysqli_query($con,$query)){
             
             session_unset();
@@ -25,9 +30,11 @@
             $t=0;
         $result=mysqli_query($con,$query);
         while($a=mysqli_fetch_assoc($result)){
-            if($_SESSION['user']==$a['User_id'] && $_SESSION['pass']==$a['Password']){
+            if($_SESSION['aadhar']==$a['Aadhar_id'] && $_SESSION['dob']==$a['DOB'] && $_SESSION['pass']==$_SESSION['cpass']){
                 $t=1;
-                header("Location: patient.php");
+                $query="UPDATE patient SET Password=AES_ENCRYPT('$pass','AbhayGuptaPathologyLab') WHERE Aadhar_Id=$aadhar;";
+                mysqli_query($con,$query);
+                header("Location: index.php");
                 exit();
                 
             }
