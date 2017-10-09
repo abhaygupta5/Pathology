158afc20868fd937b74dfd7f26a13c0bd80b099c
Abhay Gupta <abhay.abhaygupta.gupta3@gmail.com>
2017-10-09 17:58:17 +0530

    patient done

--- patient.php
+++ patient.php
@@ -7,24 +7,12 @@
     else{
         session_unset();
         session_destroy();
-        header("Location: admin_login.php");
+        header("Location: index.php");
         exit();
             
     }
 ?>
-<?php
-    
-    $query="SELECT Aadhar_id,Password FROM Patient WHERE AES_DECRYPT(Password,'AbhayGuptaPathologyLab')='$pass'";
-    if(!mysqli_query($con,$query)){
-        $message1="<font color=red>invalid user id  or password....new user first sign in</font>";
-        echo $message1;
-    }
-    else{
-    $result=mysqli_query($con,$query);
-    }
-    while($a=mysqli_fetch_assoc($result))
-        echo "Welcome ".$a['Aadhar_id'];
-?>
+
 
 
 
